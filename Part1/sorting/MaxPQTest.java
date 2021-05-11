2b8ef32623ba1e4f953a2a84395d5514be850c9c
sliou <sliou@fontrip.com>
2021-05-11 18:04:09 +0800

    [Sorting] Priority Queue Test

---
+++ Part1/sorting/MaxPQTest.java
@@ -0,0 +1,111 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.NoSuchElementException;
+
+public class MaxPQTest<K> {
+    private K[] pq;
+    private int n;
+
+    public MaxPQTest(int capacity) {
+        pq = (K[]) new Object[capacity + 1];
+        n = 0;
+    }
+
+    public MaxPQTest() {
+        this(1);
+    }
+
+    public MaxPQTest(K[] keys) {
+        n = keys.length;
+        pq = (K[]) new Object[n + 1];
+        for (int i = 0; i < n; i++) {
+            pq[i + 1] = keys[i];
+        }
+        for (int k = n / 2; k >= 1; k--) {
+            sink(k);
+        }
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void insert(K key) {
+        if (size() == pq.length - 1)
+            resize(2 * pq.length);
+        pq[++n] = key;
+        swin(n);
+    }
+
+    private void swin(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            exch(k / 2, k);
+            k = k / 2;
+        }
+    }
+
+    public K max() {
+        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        return pq[1];
+    }
+
+    public K delMax() {
+        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        K temp = pq[1];
+        exch(1, n);
+        pq[n--] = null;
+        sink(1);
+        if ((n > 0) && (n == (pq.length - 1) / 4)) resize(pq.length / 2);
+        return temp;
+    }
+
+    private void sink(int k) {
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && less(j, j + 1)) j = j + 1; // choose the bigger one child
+            if (less(j, k)) break; // if the bigger child is less than parent, break
+            exch(j, k);
+            k = j;
+        }
+    }
+
+    private boolean less(int i, int j) {
+        return ((Comparable<K>) pq[i]).compareTo(pq[j]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        K temp = pq[i];
+        pq[i] = pq[j];
+        pq[j] = temp;
+    }
+
+    private void resize(int capacity) {
+        K[] temp = (K[]) new Object[capacity];
+        for (int i = 1; i <= n; i++) {
+            temp[i] = pq[i];
+        }
+        pq = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = StdRandom.uniform(100);
+        }
+        MaxPQTest<Integer> pq = new MaxPQTest<Integer>(arr);
+        System.out.println("Max = " + pq.max());
+        while (!pq.isEmpty()) {
+            System.out.print(pq.delMax() + " ");
+        }
+    }
+}
