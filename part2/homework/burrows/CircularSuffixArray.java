183af04d815967712f14fbdc024fe26b4fa3a1ec
shihching-home <ariel647@hotmail.com>
2021-08-06 22:21:36 +0800

    [HW5] Burrows-Wheeler encoding(目前分數為99.63, 差在inverseTransform())

--- part2/homework/burrows/CircularSuffixArray.java
+++ part2/homework/burrows/CircularSuffixArray.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 public class CircularSuffixArray {
 
     private static final int R = 256;
+    private static final int CUT_OFF = 0;
 
     private static class CircularSuffix implements Comparable<CircularSuffix> {
         private final String text;
@@ -47,7 +48,8 @@ public class CircularSuffixArray {
     }
 
     private final String s;
-    private final int[] index;
+    // private final CircularSuffix[] suffixes;
+    private int[] index;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
@@ -55,18 +57,66 @@ public class CircularSuffixArray {
             throw new IllegalArgumentException(
                     "argument for constructor CircularSuffixArray() is null");
         this.s = s;
-        index = new int[length()];
-        CircularSuffix[] circularSuffixArray = new CircularSuffix[length()];
+        CircularSuffix[] suffixes = new CircularSuffix[length()];
+        index = new int[s.length()];
         for (int i = 0; i < length(); i++) {
-            circularSuffixArray[i] = new CircularSuffix(s, i);
+            suffixes[i] = new CircularSuffix(s, i);
         }
 
         // 排序
-        Arrays.sort(circularSuffixArray);
+        sort(suffixes, 0, suffixes.length - 1, 0);
 
-        // 回填index[]
+        // 回寫index[]
         for (int i = 0; i < length(); i++)
-            index[i] = circularSuffixArray[i].start;
+            index[i] = suffixes[i].start;
+    }
+
+    // 使用Quick 3-way String sort, 且小於15項時使用insertion sort
+    private void sort(CircularSuffix[] suffixes, int lo, int hi, int d) {
+
+        // 需要檢查d > length() - 1, 否則遇到全部都一樣的字母時會進入無限迴圈
+        if (d > length() - 1)
+            return;
+
+        if (hi - lo <= CUT_OFF) {
+            insertion(suffixes, lo, hi, d);
+            return;
+        }
+
+        char v = suffixes[lo].charAt(d);
+        int lt = lo, gt = hi;
+        int i = lo + 1;
+        while (i <= gt) {
+            int t = suffixes[i].charAt(d);
+            if (t < v) exch(suffixes, lt++, i++);
+            else if (t > v) exch(suffixes, gt--, i);
+            else i++;
+        }
+
+        sort(suffixes, lo, lt - 1, d);
+        sort(suffixes, lt, gt, d + 1);
+        sort(suffixes, gt + 1, hi, d);
+    }
+
+    private void exch(CircularSuffix[] suffixes, int i, int j) {
+        CircularSuffix temp = suffixes[i];
+        suffixes[i] = suffixes[j];
+        suffixes[j] = temp;
+    }
+
+    private void insertion(CircularSuffix[] suffixes, int lo, int hi, int d) {
+        for (int i = lo + 1; i <= hi; i++) {
+            for (int j = i; j > lo && less(suffixes[j], suffixes[j - 1], d); j--)
+                exch(suffixes, j, j - 1);
+        }
+    }
+
+    private boolean less(CircularSuffix v, CircularSuffix w, int d) {
+        for (int i = d; i < length(); i++) {
+            if (v.charAt(i) < w.charAt(i)) return true;
+            if (v.charAt(i) > w.charAt(i)) return false;
+        }
+        return false;
     }
 
     // length of s
@@ -85,6 +135,9 @@ public class CircularSuffixArray {
     public static void main(String[] args) {
         String s = BinaryStdIn.readString();
         CircularSuffixArray test = new CircularSuffixArray(s);
-        System.out.println(Arrays.toString(test.index));
+        int[] index = new int[s.length()];
+        for (int i = 0; i < index.length; i++)
+            index[i] = test.index(i);
+        System.out.println(Arrays.toString(index));
     }
 }
